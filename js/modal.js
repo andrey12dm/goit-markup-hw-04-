34aa6922236e434df9f16a3670133b4416af49d4
Andrey Dmitrenko <91805979+andrey12dm@users.noreply.github.com>
2024-03-23 12:59:53 +0200

    new2

--- js/modal.js
+++ js/modal.js
@@ -13,3 +13,9 @@
 	}
 })()
 console.log('work')
+
+let menuBtn = document.querySelector('.menu-btn');
+let menu = document.querySelector('.menu');
+menuBtn.addEventListener('click', function(){
+	menu.classList.toggle('active');
+})
