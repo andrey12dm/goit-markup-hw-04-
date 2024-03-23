879b8fe439ba13369029901596219ea8fee1e12e
Andrey Dmitrenko <91805979+andrey12dm@users.noreply.github.com>
2024-03-23 20:32:04 +0200

    navber

--- js/script.js
+++ js/script.js
@@ -1,39 +1,11 @@
-const headre__burger = document.querySelector(".");
-const popup = document.querySelector("#popup");
-const body = document.body;
-
-// // Клонируем меню, чтобы задать свои стили для мобильной версии
-// const menu = document.querySelector("#menu").cloneNode(1);
-
-// При клике на иконку hamb вызываем ф-ию hambHandler
-headre__burger.addEventListener("click", hambHandler);
-
-// Выполняем действия при клике ..
-function hambHandler(e) {
-  e.preventDefault();
-  // Переключаем стили элементов при клике
-  popup.classList.toggle("open");
-  headre__burger.classList.toggle("active");
-  body.classList.toggle("noscroll");
-  renderPopup();
-}
-
-// Здесь мы рендерим элементы в наш попап
-function renderPopup() {
-  popup.appendChild(menu);
-}
-
-// Код для закрытия меню при нажатии на ссылку
-const links = Array.from(menu.children);
-
-// Для каждого элемента меню при клике вызываем ф-ию
-links.forEach((link) => {
-  link.addEventListener("click", closeOnClick);
-});
-
-// Закрытие попапа при клике на меню
-function closeOnClick() {
-  popup.classList.remove("open");
-  hamb.classList.remove("active");
-  body.classList.remove("noscroll");
+const headre__burger = document.querySelector('.headre__burger');
+if (headre__burger){
+  const nav = document.querySelector('.nav');
+  const tel = document.querySelector('.tel');
+  headre__burger.addEventListener("click", function(e){
+    headre__burger.classList.toggle('_active');
+    nav.classList.toggle('_active');
+    tel.classList.toggle('_active');
+    document.body.classList.toggle('_luck')
+  });
 }
