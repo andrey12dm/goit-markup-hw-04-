ddef77b18aff11e0f11a9484eaf12723ab61102a
Andrey Dmitrenko <91805979+andrey12dm@users.noreply.github.com>
2024-03-24 13:20:46 +0200

    newrob

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
 const headre__burger = document.querySelector('.headre__burger');
+const nav = document.querySelector('.nav');
+const tel = document.querySelector('.tel');
 if (headre__burger){
-  const nav = document.querySelector('.nav');
-  const tel = document.querySelector('.tel');
+  
+  const icon__menu = document.querySelector('.icon__menu');
   headre__burger.addEventListener("click", function(e){
     headre__burger.classList.toggle('_active');
     nav.classList.toggle('_active');
     tel.classList.toggle('_active');
+    icon__menu.classList.toggle('_active');
     document.body.classList.toggle('_luck')
   });
 }
